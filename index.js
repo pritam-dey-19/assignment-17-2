8e345eb16f3533035e409c3ff15c55f4de34da95
pritam-dey-19 <dey.pritam19@gmail.com>
2023-04-23 20:57:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("demo").innerHTML =
+  "<b>Screen width :</b> " + screen.width;
+document.getElementById("demo1").innerHTML =
+  "<b>Screen height :</b>  " + screen.height;
+let depth = screen.colorDepth;
+document.getElementById("demo3").innerHTML =
+  " <b>depth of the screen : </b> " + depth;
